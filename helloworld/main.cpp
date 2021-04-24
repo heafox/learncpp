328946e6d48a7f0539f324dd0d010ea66ba6486f
胡剑波 <940930034@qq.com>
2021-04-24 15:44:30 +0800

    Added: 增加helloworld项目

---
+++ helloworld/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+/*
+返回类型 函数名称(形参列表)
+{
+    代码块;
+}
+
+形参列表：用逗号分隔
+int argc
+char *argv[]
+
+输出语句
+std::cout
+*/
+
+/**
+ * @brief 这是一个主函数，程序的入口
+ * 
+ * @param argc 程序运行参数的数量
+ * @param argv 程序运行参数的字符数组
+ * @return int 程序运行的结果
+ */
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
